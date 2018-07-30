d321d4700225639bdc3d1355f9879b91f31e0672
rfyiamcool <rfyiamcool@163.com>
2018-07-30 09:19:50 +0800

    add bin

--- setup.py
+++ setup.py
@@ -15,15 +15,16 @@ setup(
         author_email = "rfyiamcool@163.com",
         description = "restart when files changed",
         license = "MIT",
-        packages=[
-            'src'
-        ],
-
         keywords = ["restart","fengyun"],
         url = "https://github.com/rfyiamcool",
         long_description = read('README.md'),
         install_requires=['watchdog','coloredlogs'],
-        data_files=[('', ['main.py'])],
+        packages=['watchrun'],
+        entry_points={
+        'console_scripts': [
+            'watchrun = watchrun.main:main',
+            ]
+        },
         classifiers = [
              'Development Status :: 2 - Pre-Alpha',
              'Intended Audience :: Developers',
